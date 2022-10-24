c4215f71b11157b32a1d8cf2fa81e3c52346ffbc
Pato1531 <patriciomanganella@hotmail.com>
2022-10-24 20:59:01 -0300

    modificacion de index js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let perfil1 = "Conservador";
 let perfil2 = "Moderado";
 let perfil3 = "Agresivo";
 
+const conocerActivos = [];
+const carrito =[]
 
 //Creacion de Objetos mediante CLASS
 
@@ -27,7 +29,8 @@ const activosFinancieros5 = new ActivosFinancieros ("5","Materias primas","Merca
 const activosFinancieros6 = new ActivosFinancieros ("6","Bajo porcentaje de acciones Argentinas","Bolsa de valores Argentina - Merval",perfil2);
 const activosFinancieros7 = new ActivosFinancieros ("7","Acciones Argentinas","Bolsa de valores Argentina - Merval",perfil3);
 const activosFinancieros8 = new ActivosFinancieros ("8","Cedears","Bolsa de valores Argentina - Merval",perfil3);
-const activosFinancieros9 = new ActivosFinancieros ("9","Bitcoin, Ethereum, Altcoins","Mercado de Criptomonedas",perfil3);
+const activosFinancieros9 = new ActivosFinancieros ("9","Criptomonedas","Mercado de Criptomonedas",perfil3);
+
 
 
 /* Console.log - Aplico metodo del objeto
@@ -48,20 +51,7 @@ activosFinancieros9.mostrarActivosFinacieros();
 //Creación de array de info al usuario
 
 const nuestrosActivos = [activosFinancieros1,activosFinancieros2,activosFinancieros3,activosFinancieros4,activosFinancieros5,activosFinancieros6,activosFinancieros7,activosFinancieros8,activosFinancieros9];
-
 //console.log(nuestrosActivos);
-
-//Informacion al usuario sobre los activos - Porximamente cards con una breve explicacion de cada uno
-let opcionActivos ="Sobre que activo te gustaria conocer más? - Escribí el activo"
-
-for ( activo of nuestrosActivos){
-  opcionActivos += `\n ${activo.id} - ${activo.nombre}`
-}
-
-let opcionElegida = prompt(opcionActivos);
-
-alert("Elegiste conocer más sobre "+opcionElegida);
-
 
 
 //Creación de Arrays
@@ -98,4 +88,68 @@ const servicios = [
 const buscador = servicios.filter((a) => a.precio < 2000);
 console.log(buscador);
 
+
+//DOM creando cartas en el index para servicios 
+let articuloCartas = document.getElementById("cards");
+for(const servicio of servicios){
+  let carta = document.createElement("div");
+  carta.className="card col-4"
+  carta.innerHTML += `
+  <div class="card-body">
+   <h5 class="card-title">${servicio.servicio}</h5>
+   <p class="card-text">Precio: $ ${servicio.precio}.</p>
+    <button id='btn${servicio.id}'class="btn btn-primary">Solicitar</button>
+  </div>
+  `;
+  articuloCartas.append(carta);
+
+}
+//Evento Servicios para agregar al carro
+
+servicios.forEach((servicio)=>{
+  //evento para cada boton
+  document.getElementById(`btn${servicio.id}`).addEventListener("click",function(){
+    agregarAlCarrito(servicio);
+  })
+})
+
+
+
+function agregarAlCarrito(servicioAAgregar){
+  carrito.push(servicioAAgregar);
+  console.table(carrito)
+  alert("Servicio agregado al carro con éxito!!");
+}
+
+
+
+
+
+//DOM creando cartas en el index de los activos
+let articuloActivos = document.getElementById("activos");
+for(const activo of nuestrosActivos){
+  let cartaActivo = document.createElement("div");
+  cartaActivo.className="card col-3"
+  cartaActivo.innerHTML = `
+  <div class="card-body">
+   <h5 class="card-title">${activo.nombre}</h5>
+       <a href="#" id="btn-activos" class="btn btn-primary">Conoce más</a>
+  </div>
+  `;
+  articuloActivos.append(cartaActivo);
+}
+
+
+//Informacion al usuario sobre los activos - Porximamente cards con una breve explicacion de cada uno - tocar el boton y que gire la carta
+
+
+
+
+
+
+// let opcionActivos ="Sobre que activo te gustaria conocer más? - Escribí el activo"
+
+// for ( activo of nuestrosActivos){
+//   opcionActivos += `\n ${activo.id} - ${activo.nombre}`
+// }
 
