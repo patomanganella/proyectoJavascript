eac68f4717c1e4087a0b7219045afbcdc8eef1b2
Pato1531 <patriciomanganella@hotmail.com>
2022-10-27 19:00:36 -0300

    modificacion test

--- testDelInversor.js
+++ testDelInversor.js
@@ -1,10 +1,30 @@
+//Aplicación de STORAGE Y JSON
+
+function entrar (){
+  const usu = document.getElementById("mailUsuario").value;
+  const nomUsu = document.getElementById("nombreUsuario").value;
+  console.log("Usuario Ingresado: "+usu + ". "+ " Su nombre es "+nomUsu);
+  
+
+  if((usu == "pato@mail.com" && nomUsu == "patricio") || (usu == "griselda@mail.com" && nomUsu == "griselda")){
+    alert("Usuario "+nomUsu + " "+ "registrado con éxito!");
+    localStorage.setItem(usu,nomUsu);
+   
+console.log(usuNom);
+  }else{
+    alert("Los datos ingresados son erroneos")
+  }
+
+}
+
+const boton =document.getElementById("butonUsu").addEventListener("click",function(){
+  entrar()
 
 
+});
 
-//Creación de Arrays
 
-const usuarios = [];
-console.log(usuarios);
+/*
 
 
 //Aplicacion de ciclo Usuario y contraseña con ciclo FOR
@@ -22,19 +42,124 @@ for(let i=1;i<=3;i++){
     }
 //console.log(usuarios);
 
+*/
+
+//variables creadas
 let perfil1 = "Conservador";
 let perfil2 = "Moderado";
 let perfil3 = "Agresivo";
 
+/*
+const pregunta1 =[ {
+  pregunta: "Segun estos activos, cual consideras que se ajusta a un perfil de inversor conservador",
+  respuesta:"Plazo Fijo",
+  incorrecta1:"Bonos",
+  incorrecta2:"Acciones",
+  incorrecta3:"Criptomonedas"
+},
+]
+
+const pregunta2 =[ {
+  pregunta: "Segun estos activos, cual consideras que se ajusta a un perfil de inversor moderado",
+  respuesta:"Bonos",
+  incorrecta1:"Plazo Fijo",
+  incorrecta2:"Acciones",
+  incorrecta3:"Criptomonedas"
+},
+]
+const pregunta3 =[ {
+  pregunta: "Segun estos activos, cual consideras que se ajusta a un perfil de inversor agresivo",
+  respuesta:"Criptomonedas",
+  incorrecta1:"Plazo Fijo",
+  incorrecta2:"Acciones",
+  incorrecta3:"Bonos",
+},
+]
+
+*/
+
+escogerPregunta()
+
+function escogerPregunta(n){
+
+  
+  document.getElementById("pregunta").innerHTML = pregunta.categoria;
+
+
+  
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//*************************************************************** */
+//Armado de Array de preguntas
+
+const contenedor = document.getElementById("test");
+const botonRes = document.getElementById("botonTest");
+const resultadoTest = document.getElementById("resultado");
+
+// const preguntas = [
+// {//pregunta 1
+//     id:1,
+//     pregunta: "1. ¿En que activo invertirias?",
+//     respuestas: {
+//       a: "Plazos Fijos",
+//       b: "Fondos comunes de Inversión",
+//       c: "Bonos"
+//     },
+//     opcion: ["a", "b","c"],
+// },
+// {//pregunta 2
+//   id:2,
+//   pregunta: "2. ¿Cuántos años estarias dispuesto a mantener la inversion?",
+//   respuestas: {
+//     a: "de 1 a 5 años",
+//     b: "de 5 a 10 años",
+//     c: "mas de 10 años"
+//   },
+//   opcion: ["a", "b","c"],  
+// },
+// {//pregunta 3
+// id:3,
+// pregunta: "3. ¿Qué edad tienes? ",
+// respuestas: {
+//   a: "Mayor de 18 y menor de 25 años",
+//   b: "Mayor de 25 y menor de 50 años",
+//   c: "Mayor de 50 años"
+// },
+// opcion: ["a", "b","c"],
+// }
+
+// ]
+// console.log(preguntas);
+
 
 //Aplicacion de condicional
-let pregunta1 = (prompt("¿En que activo invertirias? - Elige la opción: "+"\n a) Plazos Fijos"+"\n b) Fondos comunes de Inversión"+"\n c) Bonos"));
-let pregunta2 = parseInt((prompt(" ¿Cuántos años estarias dispuesto a mantener la inversion? (solo digita el numero)")));
-let pregunta3 = (prompt("¿Qué edad tienes? - Elige la opción "+"\n a) Mayor de 18 y menor de 25 años"+"\n b) Mayor de 25 y menor de 50 años"+"\n c) Mayor de 50 años"));
+// let pregunta1 = (prompt("¿En que activo invertirias? - Elige la opción: "+"\n a) Plazos Fijos"+"\n b) Fondos comunes de Inversión"+"\n c) Bonos"));
+// let pregunta2 = parseInt((prompt(" ¿Cuántos años estarias dispuesto a mantener la inversion? (solo digita el numero)")));
+// let pregunta3 = (prompt("¿Qué edad tienes? - Elige la opción "+"\n a) Mayor de 18 y menor de 25 años"+"\n b) Mayor de 25 y menor de 50 años"+"\n c) Mayor de 50 años"));
 
 
-// Inicio de Ciclo de Preguntas 
 
+
+
+botonRes.addEventListener('click',mostrarResultado);
+// console.log(mostrarResultado);
+
+
+/*
 if((pregunta1 =="a") && (pregunta2 <="100") && (pregunta3 !="")){
   alert("Tu perfil de inversión es "+perfil1);
 
@@ -76,7 +201,6 @@ else{
 /*
 const mostrarActivos = (activos) => "Los activos que se ajustan a tu perfil son: "+activos;
 console.log(mostrarActivos("\n - Plazos fijos" +"\n - FCI"+"\n - Cauciones"+"\n - Compra de USD"));
-*/
 
 
 
