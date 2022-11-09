66627d7bd047c466c7978910f622126ecc91143a
Pato1531 <patriciomanganella@hotmail.com>
2022-11-09 00:13:36 -0300

    modifico forms

--- contacto.js
+++ contacto.js
@@ -1,22 +1,44 @@
-let miBoton = document.getElementById
-("boton-contacto");
+const nombre = document.getElementById("name")
+const email = document.getElementById("email")
+const apellido = document.getElementById("apellido")
+const comentario = document.getElementById("comentario")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
 
-let formComentarios = document.getElementById("form-comentarios");
-
-if(campoForm == Number){
-alert("Campo invalido");
-}else{
-
-}
-
-
-
-miBoton.onclick = () => { 
-    if(formComentarios != " "){
-        Swal.fire('Consulta enviada con exito!!')
-    }else{
-        alert("ERROR")
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    parrafo.innerHTML = ""
+    if(nombre.value.length <1){
+        warnings += `El nombre no es válido <br>`
+        entrar = true
+    }
+    if(apellido.value.length <1){
+        warnings += `El apellido no es válido <br>`
+        entrar = true
+    }
+    if(!regexEmail.test(email.value)){
+        warnings += `El mail no es válido <br>`
+        entrar = true
+    }
+    if(comentario.value.length <5){
+        warnings += `El comentario está vacio <br>`
+        entrar = true
     }
 
-  };
-
+    if(entrar){
+        parrafo.innerHTML = warnings
+        
+    }else{
+        parrafo.innerHTML = warnings +  Swal.fire({
+            icon: 'success',
+            title: 'Excelente!!!',
+            text: "Consulta enviada con éxito"
+            
+          });
+          return false         
+    }
+    
+})
